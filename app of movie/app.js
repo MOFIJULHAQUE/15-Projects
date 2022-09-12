6fa0a5c911961e8e28d9e367ef956b39a7fd6016
MofijulHaque <mofijulhoque995@gmail.com>
2022-09-12 11:35:51 +0530

    12-09-2022

--- app of movie/app.js
+++ app of movie/app.js
@@ -1,29 +1,28 @@
-
-let searchbox = document.querySelector("input")
-let btn = document.querySelector("button")
-let nameBox = document.querySelector(".name")
+let searchbox = document.querySelector("input");
+let btn = document.querySelector("button");
+let nameBox = document.querySelector(".name");
 btn.addEventListener("click", function () {
-    nameBox.innerHTML = ""
+    nameBox.innerHTML = "";
     fetch(`https://api.tvmaze.com/search/shows?q=${searchbox.value}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
+        .then((response) => response.json())
+        .then((data) => {
+            // console.log(data);
             for (let i = 0; i < data.length; i++) {
-                let data1 = data[i]["show"]
+                let data1 = data[i]["show"];
                 var html = `
             <div class ="wrapper">
-            <div class="image"><img src="${data1["image"]["original"]}" alt=""></div>
+            <div class="image"><img src="${data1["image"]["original"]}" alt="">
+            </div>
             <h2>${data1["name"]}</h2>
             <p>${data1["premiered"]}</p>
-           
             <p>${data1["language"]}</p>
-            <a href="${data1["url"]}"><button class = "know">Show-Details</button></a> 
-          
-         
-        </div> 
-           `
-                nameBox.innerHTML += html
-            }
-        })
+            <a href="${data1["url"]}"><button class =
+            "know">Show-Details</button></a> 
 
-})
+        </div>
+        
+        `;
+                nameBox.innerHTML += html;
+            }
+        });
+});
